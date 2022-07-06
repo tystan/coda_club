4c40ab23654e06fda981d5be2bfe8dfe1db32944
Ty Stanford <tystan@gmail.com>
2022-07-06 20:42:36 +0930

    after 2022-07-06 meeting additions
    updated getting started with R pdf as well

--- 2022-07-06_import-data/read-data-examples.R
+++ 2022-07-06_import-data/read-data-examples.R
@@ -1,16 +1,16 @@
 
 # ---- preamble ----
 
-# https://stats.oarc.ucla.edu/other/annotatedoutput/ 
+# https://stats.oarc.ucla.edu/other/annotatedoutput/
 # https://stats.oarc.ucla.edu/other/dae/
 
 # ---- last_episode ----
 
 # https://rstudio-education.github.io/hopr/r-objects.html#data-frames
-df1 <- 
+df1 <-
   data.frame(
-    face = c("ace", "two", "six"),  
-    suit = c("clubs", "clubs", "clubs"), 
+    face = c("ace", "two", "six"),
+    suit = c("clubs", "clubs", "clubs"),
     value = c(1, 2, 3)
   )
 
@@ -26,6 +26,7 @@ df1[, "face"]
 # install.packages("haven")
 # install.packages("foreign")
 # install.packages("readr")
+# install.packages("readxl")
 # install.packages("arrow")
 
 # load packages for
@@ -41,7 +42,7 @@ getwd()
 list.files()
 
 # abs path
-"C:/Users/ty/Documents/Github/coda_club/2022-07-06_import-data/"
+"C:/Users/ty/Documents/Github/coda_club/2022-07-06_import-data/data/datadata/datadatadata/"
 "~/Github/coda_club/2022-07-06_import-data/"
 
 path.expand("~/Github/coda_club/2022-07-06_import-data/")
@@ -52,11 +53,7 @@ normalizePath("~/Github/coda_club/2022-07-06_import-data", winslash = "/")
 # relative path
 "../some_data.csv"
 normalizePath("../", winslash = "/")
-"data/data2/data3/text.txt"
-
 normalizePath("../some_data.csv", winslash = "/")
-
-
 
 # ---- reading ----
 
@@ -66,57 +63,85 @@ crime <- read.csv("~/GitHub/coda_club/2022-07-06_import-data/data/crime.csv")
 
 crime <- read_csv("2022-07-06_import-data/data/crime.csv")
 
-c(
-"sid","state","crime",
-"murder","pctmetro","pctwhite",
-"pcths","poverty","single"
+crime_no_header <-
+  read.csv(
+    "~/GitHub/coda_club/2022-07-06_import-data/data/crime_no_header.csv",
+    header = FALSE
+  )
+
+
+# tsv
+
+crime <- read.delim("~/GitHub/coda_club/2022-07-06_import-data/data/crime.txt")
+
+# general delimited
+
+crime_differnt_delim <- 
+  read_delim(
+    "2022-07-06_import-data/data/crime_differnt_delim.txt",
+    delim = "|",
+    escape_double = FALSE,
+    trim_ws = TRUE
+  )
+
+# xls/xlsx
+
+crime <- read_excel("2022-07-06_import-data/data/crime.xlsx")
+
+# stata
+
+hsb2 <- read_dta("2022-07-06_import-data/data/hsb2.dta")
+
+# sas
+
+?read_sas
+hsb2demo <- read_sas("2022-07-06_import-data/data/hsb2demo.sas7bdat", NULL)
+
+# spss
+
+hsb2 <- read_sav("2022-07-06_import-data/data/hsb2.sav")
+
+# Rdata
+
+load("2022-07-06_import-data/data/crime.RData", verbose = TRUE)
+
+# save some objects as RData file
+x <- 1
+save(
+  crime,
+  hsb2demo,
+  x,
+  file = "2022-07-06_import-data/data/multiple_objs.RData"
+)
+
+# load the saved objects
+load("2022-07-06_import-data/data/multiple_objs.RData", verbose = TRUE)
+x
+crime
+
+# for fast and compressed data you can use this file format:
+?read_parquet
+
+
+# ---- writing ----
+
+# csv
+
+?write_csv
+# ?write.csv
+
+write_csv(
+  hsb2demo,
+  "2022-07-06_import-data/data/datadata/datadatadata/test_export.csv"
 )
 
 
 # tsv
 
+?write_delim
 
-# general delimeted
+?write_sas
 
-# xls/xlsx
+?write.dta
 
-# stata
-
-
-# sas
-
-
-# spss
-
-
-
-# load
-
-cdata <- read.dta("https://stats.idre.ucla.edu/stat/data/crime.dta")
-
-
-# ---- writing ----
-
-
-# csv
-
-write_csv
-write.csv
-
-# tsv
-
-write_delim()
-
-
-write_sas()
-
-write.dta()
-
-
-
-write_parquet()
-
-
-
-save(crime, file = "2022-07-06_import-data/data/crime.RData")
-
+?write_parquet
