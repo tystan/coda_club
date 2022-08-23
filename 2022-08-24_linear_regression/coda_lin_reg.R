567d202a5f60a9ef16ce602f37bf4bdb20b1ed37
Ty Stanford <tystan@gmail.com>
2022-08-23 22:09:06 +0930

    2022-08-24 files upload

---
+++ 2022-08-24_linear_regression/coda_lin_reg.R
@@ -0,0 +1,102 @@
+
+# ---- libs ----
+
+library("dplyr")
+library("ggplot2")
+library("readr")
+library("compositions") # for creating isometric log ratios further down in script
+
+# ---- load ----
+
+### NOTE: this is the file Dot provided (not on Github)
+fairclough <- read_csv("2022-08-24_linear_regression/dat/Fairclough.csv")
+
+# ---- coda ----
+
+# create a copy of the data and simplify names
+fc <- 
+  fairclough %>%
+  rename(
+    bmi = BMI,
+    sed = Sedentary,
+    lpa = Light,
+    mvpa = MVPA 
+  )
+fc
+
+# these are the compositional variables
+comp_parts <- fc[, c("sleep", "sed", "lpa", "mvpa")]
+comp_parts # have a peak
+hist(rowSums(comp_parts)) # check they add up to 1440 minutes in the day
+
+# not all rows add up to 1440, so will proportionally adjust to make 1400
+# (ignore the code, don't need to understand)
+standardised_comp_parts <- 
+  1440 * t(apply(comp_parts, 1, function(x) x / sum(x)))
+
+# check old and new values
+cbind(comp_parts, standardised_comp_parts)
+
+# update dataset with standardised 1440 per row
+fc[, c("sleep", "sed", "lpa", "mvpa")] <- standardised_comp_parts
+
+
+# ---- attempt_at_model ----
+
+# now fit a model for the outcome of BMI using the compositional variables
+fc %>% select(bmi, sleep, sed, lpa, mvpa)
+fc_mod <- lm(bmi ~ sleep + sed + lpa + mvpa, data = fc)
+summary(fc_mod) # why is there NAs in there?
+
+
+# ---- workaround1 ----
+
+# fit the model (substitution model) without "mvpa"
+# how does this change the interpretation of the coefficients?
+fc_mod2 <- lm(bmi ~ sleep + sed + lpa, data = fc)
+summary(fc_mod2)
+
+# fit the model (substitution model) without "sleep"
+fc_mod3 <- lm(bmi ~ sed + lpa + mvpa, data = fc)
+summary(fc_mod3)
+
+
+# ---- workaround2 ----
+
+# use isometric log-ratios
+fc_ilrs <-
+  fc %>%
+  select(bmi, sleep, sed, lpa, mvpa)
+
+# this is creating an ilr basis
+V <-
+  t(matrix(
+    c(
+      +1, -1, -1, -1,
+       0, +1, -1, -1,
+       0,  0, +1, -1
+    ),
+    byrow = TRUE,
+    nrow = 3
+  ))
+psi <- compositions::gsi.buildilrBase(V)
+
+# create ilrs using the compositions package (and psi above)
+acomp_vals <- acomp(fc_ilrs[, c("sleep", "sed", "lpa", "mvpa")], total = 1)
+ilr_vals <- ilr(acomp_vals, V = psi)
+
+# update column names of ilrs
+colnames(ilr_vals) <- paste0(".", 1:3)
+
+# add on ilrs to data.frame
+fc_ilrs$ilr <- ilr_vals
+# do the ilrs make sense compared to the original composition vars?
+fc_ilrs
+
+# now fit an ilr model: bmi ~ ilr.1 + ilr.2 + ilr.3
+fc_mod4 <- lm(bmi ~ ilr, data = fc_ilrs)
+summary(fc_mod4)
+anova(fc_mod4)
+
+
+
---
+++ 2022-08-24_linear_regression/linear_reg.R
@@ -0,0 +1,239 @@
+
+
+# ---- preamble -----
+
+### Simple Linear Regression
+# Breakdown:
+# Simple = one (predictor) variable
+# Linear = fitting a straight line
+# Regression = fancy/stats way of saying quantifying a relationship between variables
+
+### approximate synonyms [will try to use first one for consistency]
+# outcome variable == outcome == response variable == dependent variable
+# predictor variable == predictor == independent variable
+# parameter estimates == coefficients (which are the intercept/slope)
+
+### simple linear regression models are sometimes presented like this
+# y_i = β_0 + β_1 x_i + e_i
+# or
+# y_i = b0 + b1 x_i + e_i
+
+### remind you of early high school maths?
+# y = m x + c  
+# (just an equation of a straight line)
+
+### here: 
+# y is outcome (in dataset), 
+# x is predictor (in dataset),
+# m = b1, (slope) [model estimates this from dataset]
+# c = b0, (intercept) [model estimates this from dataset]
+
+### BUT NOTE all of these characters/letters are just place holders for whatever
+### variables we are interested in, e.g.,
+# BMI              = (intercept) + (slope) x exercise_in_hours,    or
+# blood_pressure   = (intercept) + (slope) x dietary_salt_intake,  or
+# depression_score = (intercept) + (slope) x exercise_in_hours
+
+### to fit all of the models specified above, the syntax for R is simple:
+# lm(y ~ x, data = data_containing_xy)
+# [where y is response/outcome and x is predictor/independent variable]
+
+# the above regression model estimates the (intercept) and (slope) of
+# y = (intercept) + (slope) x exercise_in_hours
+#
+# <also the e_i from before, but let's keep it simple>
+
+### let's get into the examples and we can revisit the above
+
+
+# ---- libs ----
+
+library("dplyr")
+library("ggplot2")
+# install.packages("betareg")
+library("betareg") # using this for the "StressAnxiety" dataset
+
+
+# ---- example_data_1 ----
+
+# load data
+data("StressAnxiety", package = "betareg")
+as_tibble(StressAnxiety)
+
+
+stress_dat <- # create a copy we can play with (and changing units)
+  as_tibble(StressAnxiety) %>%
+  # makes percentages of Depression Anxiety Stress Scales
+  mutate(anxiety = 100 * anxiety, stress = 100 * stress) 
+
+stress_dat
+
+### details from ?StressAnxiety
+# Stress and anxiety among nonclinical women in Townsville, Queensland, Australia
+# 166 observations on 2 variables
+# ---
+# Smithson, M., and Verkuilen, J. (2006). A Better Lemon Squeezer? 
+# Maximum-Likelihood Regression with Beta-Distributed Dependent Variables.
+# Psychological Methods, 11(7), 54–71.
+
+# ---- explore_data ----
+
+
+# before doing any analysis -- always explore the data!!
+# all models have assumptions, knowing what the data look like is a sanity check
+
+# summary stats:
+summary(stress_dat)
+
+stress_dat %>%
+  summarise(
+    mean_stress = mean(stress),
+    sd_stress = sd(stress),
+    mean_anxiety = mean(anxiety),
+    sd_anxiety = sd(anxiety)
+  )
+
+
+# what about a plot?
+
+stress_dat %>%
+  ggplot(., aes(x = stress, y = anxiety)) +
+  geom_point(alpha = 0.5) +
+  theme_bw()
+
+(jitter_plot <-
+  stress_dat %>%
+  ggplot(., aes(x = stress, y = anxiety)) +
+  geom_jitter(alpha = 0.5, width = 5, height = 0) +
+  theme_bw())
+
+
+cor(stress_dat)
+
+
+# ---- fit_lm ----
+
+# linear model a good idea?
+
+mod1 <- lm(anxiety ~ stress, data = stress_dat)
+summary(mod1)
+# Call:
+# lm(formula = anxiety ~ stress, data = stress_dat)
+#
+# Residuals:
+#     Min      1Q  Median      3Q     Max 
+# -26.755  -5.502  -1.366   4.159  31.585 
+#
+# Coefficients:
+#             Estimate Std. Error t value Pr(>|t|)    
+# (Intercept) -3.64188    1.23626  -2.946  0.00369 ** 
+# stress       0.48303    0.03776  12.791  < 2e-16 ***
+# ---
+# Signif. codes:  0 ‘***’ 0.001 ‘**’ 0.01 ‘*’ 0.05 ‘.’ 0.1 ‘ ’ 1
+
+# Residual standard error: 9.404 on 164 degrees of freedom
+# Multiple R-squared:  0.4994,	Adjusted R-squared:  0.4963 
+# F-statistic: 163.6 on 1 and 164 DF,  p-value: < 2.2e-16
+
+
+### some questions to explain the output:
+# (1) Is `stress` a significant predictor of `anxiety`?
+2e-16 < 0.05
+
+# (2) If `stress` increases, does the model predict `anxiety` will increase or 
+#   decrease (on average)?
+
+# (3) what is the estimated slope (b1 or m) coefficient?
+
+# (4) What is the estimated intercept (b0 or c) coefficient?
+
+# (5) For every 1 unit increase in `stress`, 
+#   what is the estimated increase/decrease in `anxiety`?
+
+# (6) What is the interpretation of the estimated intercept coefficient?
+
+# (7) What is the estimated regression line/formula?
+
+# anxiety =  intercept + slope * stress
+
+jitter_plot +
+  geom_abline(intercept = -3.64188, slope = 0.48303, col = "dodgerblue")
+
+# (8) What is the predicted `anxiety` for a `stress` value of 35?
+
+predict(mod1, newdata = data.frame(stress = 35))
+
+# (9) What is the model R^2?
+
+
+
+
+# ---- check_assumptions ----
+
+
+### checking the model assumptions:
+# 1. Independence of observations
+# 2. Linearity: look at residuals vs fitted
+# 3. Homoscedasticity: look at scale-location
+# 4. Normality: look at Q-Q plot
+
+par(mfrow = c(2, 2))
+plot(mod1)
+par(mfrow = c(1, 1))
+
+
+
+
+
+# ---- not_covered ----
+
+
+# what if the outcome variable isn't continuous?
+# what if one of the predictors is not continuous (i.e., discrete)?
+# is it sensible to say x affects y (does y affect x, or even are they just correlated?)
+# model selection (remove or add terms) or comparing competing models
+# testing multiple variables at once (anova() function)
+# observational data vs experimental (and "covariates")
+# repeated measures
+
+
+
+
+
+# ---- multiple_linear_regression ----
+
+# it's the same idea but just more predictor variables (and thus coefficients)
+# y_i = β_0 + β_1 x_ {1i} + β_2 x_{2i} + ... + β_p x_{pi} + e_i
+
+# adding another variable (fake but for illustration later on)
+set.seed(1234567)
+stress_dat <-
+  stress_dat %>%
+  mutate(
+    income = rchisq(n = n(), df = 50 / (anxiety / 100 + 1 + runif(n())))
+  )
+
+mod2 <- lm(anxiety ~ stress + income, data = stress_dat)
+summary(mod2)
+
+stress_dat %>%
+  ggplot(., aes(x = stress, y = anxiety, col = income)) +
+  geom_jitter(alpha = 0.75, width = 5, height = 0, size = 2.5) +
+  scale_color_distiller(palette = "Spectral") + 
+  theme_bw()
+
+### checking the model assumptions is the same:
+# 1. Independence of observations
+# 2. Linearity: look at residuals vs fitted 
+#    (technically should check residuals vs each predictor variable too)
+# 3. Homoscedasticity: look at scale-location
+# 4. Normality: look at Q-Q plot
+
+par(mfrow = c(2, 2))
+plot(mod2)
+par(mfrow = c(1, 1))
+
+
+
+
+
