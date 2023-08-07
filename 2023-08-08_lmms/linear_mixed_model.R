4973e10886c61bff4d8762f12e99545c1f1ed87c
Ty Stanford <tystan@gmail.com>
2023-08-07 22:04:11 +0930

    Update linear_mixed_model.R

--- 2023-08-08_lmms/linear_mixed_model.R
+++ 2023-08-08_lmms/linear_mixed_model.R
@@ -1,9 +1,18 @@
 
+# ---- libs ----
+
+
 # also require "nlme" for the data
-library("lme4")
+library("lme4")     # linear mixed effect model package
+library("lmerTest") # adds p-values to summary of lmer() models
 library("tidyr")
-library("dplyr")
-library("ggplot2")
+library("dplyr")    # data manipulation and pipe (`%>%`)
+library("ggplot2")  # plotting
+library("ggthemes") # colour palettes for ggplot
+
+
+
+# ---- the_data ----
 
 # Investigators at the University of North Carolina Dental School 
 # followed the growth of 27 children (16 males, 11 females) 
@@ -11,22 +20,121 @@ library("ggplot2")
 # distance between the pituitary and the pterygomaxillary fissure (in mm), 
 # two points that are easily identified on x-ray exposures of the 
 # side of the head.
+
+# load data
 data(Orthodont, package = "nlme")
+
+# peak at first 10 rows
 head(Orthodont, n = 10)
 
-(n_orth <- nrow(Orthodont))
-(n_partic <- length(unique(Orthodont$Subject)))
 
+# ---- subset_data ----
 
-Orthodont %>%
-  ggplot(., aes(x = age, y = distance, group = Subject, col = Sex)) +
-  geom_line(alpha = 0.5) +
+orth_f <-
+  Orthodont %>%
+  as_tibble(.) %>%
+  dplyr::filter(Sex == "Female") %>%
+  mutate(Subject = as.character(Subject))
+
+orth_f
+
+# sample size
+(n_partic <- length(unique(orth_f$Subject)))
+(n_orth <- nrow(orth_f))
+
+# have a look at data
+# should always do version of this berfore fitting models!
+orth_f %>%
+  ggplot(., aes(x = age, y = distance, col = Subject)) +
+  facet_wrap(~ Subject, ncol = 3) +
   geom_point() +
-  facet_wrap( ~ Subject) +
+  scale_colour_tableau(palette = "Tableau 20") +
   theme_bw() 
 
 
+# ---- fit_standard_linear_regression ----
 
+### what if we fit a linear regression as before? Why is this not appropriate?
+
+summary(lm(distance ~ age, data = orth_f))
+
+# fyi this is variance of difference between observed and predicted
+2.164 ^ 2 
+sigma(lm(distance ~ age, data = orth_f)) ^ 2 # extraced from model instead of output
+
+
+# ---- fit_linear_mixed_effects ----
+
+summary(m1_f <- lmer(distance ~ age + (1 | Subject), data = orth_f))
+# fyi this is the total variance of difference between observed and 
+# (population/fixed) predicted
+2.068 ^ 2 + 0.780 ^ 2 
+
+
+# add LME values to original dataset (ignore, just for plotting)
+orth_f <- 
+  orth_f %>% 
+  mutate(
+    ### SAME: 
+    # pop_pred = 17.37273 + 0.47955 * age
+    pop_pred = predict(m1_f, re.form = ~ 0)
+  )
+orth_f_ranef <-
+  tibble(
+    Subject = rownames(ranef(m1_f)$Subject),
+    ran_int = ranef(m1_f)$Subject[["(Intercept)"]]
+  )
+orth_f <- 
+  inner_join(orth_f, orth_f_ranef, "Subject") %>%
+  mutate(resid_err = distance - (pop_pred + ran_int))
+
+
+# ---- connect_data_and_model_output ----
+
+
+# store a plot of the data
+fem_plot <-
+  orth_f %>%
+  ggplot(., aes(x = age, y = distance, col = Subject)) +
+  geom_point() +
+  scale_colour_tableau(palette = "Tableau 20") +
+  theme_bw() 
+
+# "print" basic plot
+fem_plot
+
+# now let's add a (population/fixed) predicted line
+fem_plot +
+  geom_line(aes(y = pop_pred, group = Subject), size = 3, col = "black")
+
+
+# now have random intercept offsets for each subject 
+# as well as (population/fixed) predicted line
+fem_plot +
+  annotate(
+    geom = "segment", 
+    x = 8, y = 17.37273 + 0.47955 * 8,
+    xend = 14, yend = 17.37273 + 0.47955 * 14,
+    size = 3
+  ) +
+  geom_line(aes(y = pop_pred + ran_int, group = Subject)) 
+
+
+### question: how does the model values relate to the summary output?!?
+
+summary(m1_f)
+# fixef intercept = 17.37273
+# fixef slope =  0.47955
+# ranef intercept sd: 2.068
+orth_f_ranef %>% summarise(ranef_sd = sd(ran_int))
+# ranef residual error sd: 0.780
+orth_f %>% summarise(resid_sd = sd(resid_err))
+
+
+
+
+
+# ---- different_models_for_full_data ----
 
 
 
