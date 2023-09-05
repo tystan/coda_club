156f5e9b93ccae9fd461599318c6439c17578d20
Ty Stanford <tystan@gmail.com>
2023-09-05 21:18:46 +0930

    Added extra comments to code

--- 2023-09-05_power_samp_size/intro_to_power_calcs.R
+++ 2023-09-05_power_samp_size/intro_to_power_calcs.R
@@ -18,9 +18,31 @@ sigma <- 5
 d <- delta / sigma # "effect-size"
 
 
-
 # we want to check whether people in Adelaide aged 20-29 spend more (or different) 
 # amount of time in physical activity than people in Adelaide aged 30-39
+
+# a power calc for a fixed number of people
+(power_result <- pwr.t.test(
+  n = npg, 
+  d = delta / sigma, 
+  sig.level = 0.05, 
+  power = NULL, 
+  type = "two.sample",
+  alternative = "two.sided"
+))
+
+
+# a fixed power to find required n (per group)
+pwr.t.test(
+  n = NULL, 
+  d = delta / sigma, 
+  sig.level = 0.05, 
+  power = 0.95, 
+  type = "two.sample",
+  alternative = "two.sided"
+)
+
+
 
 # so we randomly sample 20 people from each age group and get the following
 # 7-day average daily physical activity times
@@ -34,7 +56,18 @@ pa_dat <-
   )
 pa_dat
 
+
+### this side of the divider we don't have access to in our studies
+### (the above is just a thought experiment about how our data might
+### "come to be")
+
+
 # -------------------------------------------------------------------
+# -------------------------------------------------------------------
+
+
+### this side of the divider is the data we collect to in our studies
+### (and we don't know if either H_0 or H_A is the truth)
 
 # so to check our above question we could do a t-test:
 boxplot(pa ~ age_cat, data = pa_dat)
@@ -48,39 +81,21 @@ t.test(pa ~ age_cat, data = pa_dat, var.equal = TRUE)$p.value
 t.test(pa ~ age_cat, data = pa_dat, var.equal = TRUE)$p.value < 0.05
 
 
+### let's repeat the above lots and lots to see how often we reach "significance" as a proportion
 
-pwr.t.test(
-  n = npg, 
-  d = delta / sigma, 
-  sig.level = 0.05, 
-  power = NULL, 
-  type = "two.sample",
-  alternative = "two.sided"
-)
-
-
-
-pwr.t.test(
-  n = NULL, 
-  d = delta / sigma, 
-  sig.level = 0.05, 
-  power = 0.95, 
-  type = "two.sample",
-  alternative = "two.sided"
-)
-
-
-
-reps <- 10000
+reps <- 1000
 (is_sig <- rep(NA, reps))
-# is_sig[3] <- "fdglskjhbgdf"
+# is_sig[3] <- "test" # inserting a value in the third spot in vector for demonstration
 # is_sig
 for (i in 1:reps) { # i <- 1
+  # create new randomly generated dataset
   pa_dat <-
     tibble(
       age_cat = rep(c("grp20", "grp30"), each = npg),
       pa = c(rnorm(npg, 25, sigma), rnorm(npg, 25 - delta, sigma))
     )
+  # is the t-test significant -- do we reject H0?
+  ### remember: H_A is true so we DO want significance (i.e. reject H_A)
   is_sig[i] <- t.test(pa ~ age_cat, data = pa_dat, var.equal = TRUE)$p.value < 0.05
 }
 # is_sig
@@ -97,7 +112,7 @@ plot(
   ylab = "power estimate",
   xlab = "Number of simulations"
 )
-abline(h = 0.4560341, col = "orange") # orange line is pwr.t.test() power result
+abline(h = power_result$power, col = "orange") # orange line is pwr.t.test() power result
 
 
 
@@ -130,14 +145,16 @@ t.test(x = pa_age20s, y = pa_age30s, var.equal = TRUE)
 
 reps <- 100
 (is_sig <- rep(NA, reps))
-# is_sig[3] <- "fdglskjhbgdf"
-# is_sig
-for (i in 1:reps) { # i <- 1
+
+for (i in 1:reps) { 
+  # create new randomly generated dataset
   pa_dat <-
     tibble(
       age_cat = rep(c("grp20", "grp30"), each = npg),
       pa = c(rnorm(npg, 25, sigma), rnorm(npg, 25, sigma))
     )
+  # is the t-test significant -- do we reject H0?
+  ### remember: H_0 is true so we DO NOT want significance
   is_sig[i] <- t.test(pa ~ age_cat, data = pa_dat, var.equal = TRUE)$p.value < 0.05
 }
 # is_sig
